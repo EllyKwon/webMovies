c9ccb082e7f0f7344e5199bb01fbf5f494ca2a8a
tok_o <tok_o@naver.com>
2023-01-16 01:17:55 +0900

    프로젝트 초기 세팅

---
+++ src/test/java/com/WebMovies/WebMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
