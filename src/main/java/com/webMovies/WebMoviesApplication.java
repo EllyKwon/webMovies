db859a71b25dd7ee8a14b9009237fb5a7f8fb47a
tok_o <tok_o@naver.com>
2023-01-31 01:05:14 +0900

    일부 오류 수정 및 war 배포를 위해 설정 변경

--- src/main/java/com/webMovies/WebMoviesApplication.java
+++ src/main/java/com/webMovies/WebMoviesApplication.java
@@ -2,9 +2,16 @@ package com.webMovies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebMoviesApplication {
+public class WebMoviesApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WebMoviesApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebMoviesApplication.class, args);
